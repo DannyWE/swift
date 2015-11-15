512c47f73038744fb351942623d4353d769d9fe7
danny.dong <danny.dong@rea-group.com>
2015-11-16 10:29:44 +1100

    initial commit

---
+++ swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift
+//
+//  Created by Danny Dong on 16/11/2015.
+//  Copyright © 2015 XIAOHE Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
